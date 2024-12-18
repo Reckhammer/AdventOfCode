32b2131ef7942cbc59ba74b561fd555c485b514d
Reckhammer <joshua.rechkemmer48@gmail.com>
2024-12-18 00:13:53 -0600

    Initial Day1 push

---
+++ 2024/Python/Day1.py
@@ -0,0 +1,13 @@
+# Advent of Code 2024
+# Day 1 - Historian Hysteria
+# Problem link: https://adventofcode.com/2024/day/1
+# Problem: Given two lists of numbers, find the distance between the nth 
+#   smallest numbers from each of the lists. Sum up and return the total 
+#   difference
+
+# Python Solution
+# Read the lists from the input .txt file
+# Sort the lists
+# Iterate both lists and add the difference to a total
+
+
